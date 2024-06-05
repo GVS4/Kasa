2fabf58c92f46a95482bd42ddbbb9a7f21f204e4
GVS <johndoe@exemple.com>
2024-06-05 14:54:17 +0200

    github pages : correction 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./sass/main.scss";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/P8">  {/* Ajoutez le basename ici */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
